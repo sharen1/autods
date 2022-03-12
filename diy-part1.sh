f03a7b1d85a96b028263776a7dd32b6aa83d4811
sharen1 <97437780+sharen1@users.noreply.github.com>
2022-03-11 22:12:36 -0600

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/ntlf9t/luci-app-easymesh' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/sharen1/luci-app-easymesh package/luci-app-easymesh
